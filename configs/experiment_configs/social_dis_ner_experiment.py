5be52227adb42a6086778a123d3145665bb30043
flyingmothman <flyingmothman>
2023-05-10 18:00:06 -0700

    add social dis ner experiment config

---
+++ configs/experiment_configs/social_dis_ner_experiment.py
@@ -0,0 +1,68 @@
+from utils.config import AdafactorModifier, AdamModifier, EpochsCustomModifier, SmallerSpanWidthModifier, TinySpanWidthModifier, get_experiment_config, Epochs20Modifier, BiggerBatchModifier, TestEveryEpochModifier
+
+
+experiments = [
+    # SPAN
+    get_experiment_config(
+        model_config_module_name='model_span_large_default',
+        dataset_config_name='social_dis_ner_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            SmallerSpanWidthModifier(),
+            TestEveryEpochModifier(),
+            AdafactorModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_span_large_default',
+        dataset_config_name='social_dis_ner_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            SmallerSpanWidthModifier(),
+            TestEveryEpochModifier(),
+            AdamModifier()
+        ]
+    ),
+
+
+    # SEQ
+    get_experiment_config(
+        model_config_module_name='model_seq_large_default',
+        dataset_config_name='social_dis_ner_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            TestEveryEpochModifier(),
+            AdafactorModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_seq_large_default',
+        dataset_config_name='social_dis_ner_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            TestEveryEpochModifier(),
+            AdamModifier()
+        ]
+    ),
+
+
+    # CRF
+    get_experiment_config(
+        model_config_module_name='model_seq_large_crf',
+        dataset_config_name='social_dis_ner_vanilla',
+        modifiers=[
+            EpochsCustomModifier(num_epochs=25),
+            TestEveryEpochModifier(),
+            AdafactorModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_seq_large_crf',
+        dataset_config_name='social_dis_ner_vanilla',
+        modifiers=[
+            EpochsCustomModifier(num_epochs=25),
+            TestEveryEpochModifier(),
+            AdamModifier()
+        ]
+    ),
+]
