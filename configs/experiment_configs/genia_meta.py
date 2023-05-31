34dfcb8da07a649f6a5d9ac77d467c9ff7212707
harshshredding <harshshredding@gmail.com>
2023-05-31 16:33:40 -0700

    add remaining meta experiments

---
+++ configs/experiment_configs/genia_meta.py
@@ -0,0 +1,16 @@
+from utils.config import BiggerBatchModifier, EpochsCustomModifier, get_experiment_config, AccuracyEvaluationModifier, TestEveryEpochModifier, AdamModifier
+
+
+experiments = [
+    get_experiment_config(
+        model_config_module_name='meta',
+        dataset_config_name='genia_meta',
+        modifiers=[
+            EpochsCustomModifier(num_epochs=15),
+            TestEveryEpochModifier(),
+            AdamModifier(),
+            AccuracyEvaluationModifier(),
+            BiggerBatchModifier(),
+        ]
+    ),
+]
